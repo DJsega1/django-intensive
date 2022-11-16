a0081c7899cb5311454f905963165dcd2126addc
DJsega1 <DJsega1@yandex.ru>
2022-11-16 20:43:50 +0300

    fixed some issues

--- homepage/views.py
+++ homepage/views.py
@@ -6,7 +6,6 @@ def home(request):
     template = 'homepage/index.html'
     MAX_TEXT_WORDS = 10
     items = Item.objects.published().filter(is_on_main=True)
-    # ' '.join(text.split(' ', MAX_TEXT_WORDS))[:-1]
     context = {
         'MAX_TEXT_WORDS': MAX_TEXT_WORDS,
         'items': items
