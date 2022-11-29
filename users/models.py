d5fcf021375dea8888b2e2420388d9bf89d12d19
DJsega1 <DJsega1@yandex.ru>
2022-11-29 22:29:49 +0300

    Fixed issue

--- users/models.py
+++ users/models.py
@@ -25,20 +25,17 @@ class User(AbstractUser):
         null=True,
         help_text='Не более 150 символов. Буквы, цифры и @/./+/-/_ .',
         validators=[
-            UnicodeUsernameValidator
+            UnicodeUsernameValidator,
         ],
-        error_messages={
-            'unique': 'Пользователь с таким адресом уже существует',
-        },
     )
     birthday = models.DateField(
-            blank=True,
-            null=True,
-            verbose_name='день рождения',
-            validators=[
-                MinValueValidator(limit_value=date(1900, 1, 1)),
-                MaxValueValidator(limit_value=date.today())
-            ],
+        blank=True,
+        null=True,
+        verbose_name='день рождения',
+        validators=[
+            MinValueValidator(limit_value=date(1900, 1, 1)),
+            MaxValueValidator(limit_value=date.today())
+        ],
     )
     USERNAME_FIELD = 'email'
     EMAIL_FIELD = 'email'
